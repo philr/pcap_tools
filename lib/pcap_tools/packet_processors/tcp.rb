4bc0caf0b7aa8cca1a132c4e9b80c34820caab4f
Phil Ross <phil.ross@gmail.com>
2015-11-22 23:23:02 +0000

    Don't discard data in TCP segments with the FIN flag set.
    A TCP segment with a FIN flag may contain data. The FIN flag is
    considered to occur after the last data octet of the segment in which it
    occurs.

--- lib/pcap_tools/packet_processors/tcp.rb
+++ lib/pcap_tools/packet_processors/tcp.rb
@@ -22,24 +22,21 @@ module PcapTools
             :data => [],
             :tcp_lost_segment => false,
           }
-        elsif packet[:tcp_flags][:fin] || packet[:tcp_flags][:rst]
-          if @streams[stream_index]
+        elsif @streams[stream_index]
+          tcp_flags = packet[:tcp_flags]
+          packet[:type] = (packet[:from] == @streams[stream_index][:first][:from] && packet[:from_port] == @streams[stream_index][:first][:from_port]) ? :out : :in
+          packet.delete :tcp_flags
+          @streams[stream_index][:data] << packet if packet[:size] > 0
+          if packet[:tcp_lost_segment]
+            @streams.delete stream_index
+            $stderr.puts "Ignoring tcp stream #{stream_index}, tcp segments are missing"
+          elsif tcp_flags[:fin] || tcp_flags[:rst]
             current = {:index => stream_index, :data => @streams[stream_index][:data]}
             @stream_processors.each do |p|
               current = p.process_stream current
               break unless current
             end
             @streams.delete stream_index
-          end
-        else
-          if @streams[stream_index]
-            packet[:type] = (packet[:from] == @streams[stream_index][:first][:from] && packet[:from_port] == @streams[stream_index][:first][:from_port]) ? :out : :in
-            packet.delete :tcp_flags
-            @streams[stream_index][:data] << packet if packet[:size] > 0
-            if packet[:tcp_lost_segment]
-              @streams.delete stream_index
-              $stderr.puts "Ignoring tcp stream #{stream_index}, tcp segments are missing"
-            end
           end
         end
       end
